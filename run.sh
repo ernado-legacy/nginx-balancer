400a9a36ce15715e3e955a9b8ad39b4518d0f5e8
ernado <ernado@ya.ru>
2014-10-27 04:02:35 +0300

    minified run.sh

--- run.sh
+++ run.sh
@@ -7,21 +7,7 @@ export HOST_IP=${HOST_IP:-172.17.42.1}
 export ETCD=$HOST_IP:$ETCD_PORT
 export ETCDCTL_PEERS="http://$ETCD"
 
-echo "[nginx-balancer] booting container. ETCD: $ETCD"
-
-etcdctl --peers="http://$ETCD" ls
-
-# Try to make initial configuration every 5 seconds until successful
-until confd -onetime; do
-    echo "[nginx-balancer] waiting for confd to create initial nginx configuration"
-    sleep 1
-done
-
-# Put a continual polling `confd` process into the background to watch
-# for changes every 10 seconds
 confd -interval 10 -watch=true &
 echo "[nginx-balancer] confd is now monitoring etcd for changes"
-
-# Start the Nginx service using the generated config
 echo "[nginx-balancer] starting nginx"
 nginx
